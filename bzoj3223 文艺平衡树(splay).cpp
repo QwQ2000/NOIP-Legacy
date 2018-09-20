bf4395f35f33755e6a43edefcf6125aee8b19ea5
QwQ2000 <396707050@qq.com>
2018-09-20 20:04:36 +0800

    Update and rename 111 to bzoj3223 文艺平衡树(splay).cpp

--- bzoj3223 文艺平衡树(splay).cpp
+++ bzoj3223 文艺平衡树(splay).cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-const int N=10005;
+const int N=100005;
 
 struct Node {
 	int v,sz,flag;
@@ -25,10 +25,9 @@ struct Node {
 	int dir() {
 		return fa->ch[1]==this;
 	}	
-}pool[N],*null;
+}pool[N],*null,*root;
 
 int n=0,m=0;
-int a[N];
 int cnt=1;
 int ans[N];
 
@@ -50,6 +49,7 @@ void build(int l,int r,Node* fa,int son) {
 }
 
 inline void init() {
+	root=pool+1;
 	null=pool;
 	null->v=null->sz=null->flag=0;
 	null->fa=null->ch[0]=null->ch[1]=null;
@@ -57,49 +57,71 @@ inline void init() {
 } 
 
 void dfs(Node* p) {
-	if (p!=null) {
+	if (p!=null) {	
 		p->pushDown();
 		dfs(p->ch[0]);
-		ans[cnt++]=a[p->v];
+		if (p->v!=0 && p->v!=n+1)
+			ans[cnt++]=p->v;
 		dfs(p->ch[1]);
 	}
 }
 
 inline void print() {
-	cnt=0;
-	dfs(pool+1);
+	cnt=1;
+	dfs(root);
 	for (int i=1;i<=n;++i)
 		printf(i==n?"%d\n":"%d ",ans[i]);	
 }
 
 inline void rotate(Node* p) {
-	
+	Node *fa=p->fa,*gr=fa->fa;
+	int d1=p->dir(),d2=fa->dir();
+	p->fa=gr;
+	if (gr!=null) 
+		gr->ch[d2]=p;
+	fa->ch[d1]=p->ch[d1^1];
+	p->ch[d1^1]->fa=fa;
+	p->ch[d1^1]=fa;
+	fa->fa=p;
+	if (p->fa==null)
+		root=p;
+	fa->pushUp();
+	p->pushUp();
 }
 
 void splay(Node* p,Node* fa) {
+	p->pushDown();
 	while (p->fa!=fa) {
 		if (p->fa->fa==fa)
 			rotate(p);
-		else {
-			if (p->fa)
+		else if (p->fa->dir()==p->dir()) {
+			rotate(p->fa);
+			rotate(p);
+		} else {
+			rotate(p);
+			rotate(p);
 		}
 	}
 }
 
-int find() {
-	
+Node* find(int x,Node* cur) {
+	cur->pushDown();
+	if (cur->ch[0]->sz==x)
+		return cur;
+	else if (cur->ch[0]->sz<x)
+		find(x-cur->ch[0]->sz-1,cur->ch[1]);
+	else
+		find(x,cur->ch[0]);
 }
 
 inline void reverse(int l,int r) {
-	
-	splay(find(l-1),null);
-	splay(find(r+1),)
+	splay(find(l-1,root),null);
+	splay(find(r+1,root),root);
+	root->ch[1]->ch[0]->flag^=1;
 }
 
 int main(void) {
 	scanf("%d %d",&n,&m);
-	for (int i=1;i<=n;++i)
-		scanf("%d",a+i);
 	init();	
 	while (m--) {
 		int l=0,r=0;
